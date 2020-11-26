c1863e06b6239a3d67348fedacdffdeb84c60476
Rustem Nasyrov <nasyroff.rus@gmail.com>
2020-11-26 21:40:20 +0500

    Проба пера с шаблонами (DOM)

---
+++ src/components/galleryWrap.template.js
@@ -0,0 +1,15 @@
+'use strict';
+
+export default `
+    <div class="vision-photo-gallery">
+        <div class="vision-photo-gallery__item main-photo">
+            main photo
+        </div>    
+        <div class="vision-photo-gallery__item gallery">
+            gallery
+            gallery
+            gallery
+            gallery
+        </div>    
+    </div>
+`
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+import * as wrapperTemplate from './components/galleryWrap.template';
+
+class VisionPhotoGallery {
+    /**
+     * @param renderTo {string} - Элемент, куда будет сгененрирована галлерея.
+     */
+    constructor(renderTo) {
+        const rootElement = document.getElementById(renderTo);
+
+        rootElement.innerHTML = wrapperTemplate.default;
+
+    }
+}
+
+new VisionPhotoGallery('app')
