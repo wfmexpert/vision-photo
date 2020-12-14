61468de658d84328bdb017d3faea99df3c060c2e
rustem-nasyrov <nasyroff.rus@gmail.com>
2020-12-14 19:21:34 +0500

    Экспорт

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './VisionPhotoGallery/index.mjs',
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         compress: false,
